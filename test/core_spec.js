c3c25c01f6fc78807cdf13e2ed7d144e6d02977b
ryym <ryym.64@gmail.com>
2015-12-19 21:01:56 +0900

    Write func to set entries

---
+++ test/core_spec.js
@@ -0,0 +1,29 @@
+import { List, Map } from 'immutable';
+import { expect }    from 'chai';
+import { setEntries } from '../src/core';
+
+describe('Application logic', () => {
+
+  describe('setEntries', () => {
+    
+    it('adds the entries to the state', () => {
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+    it('converts anything iterable to immutable', () => {
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+  });
+
+});
