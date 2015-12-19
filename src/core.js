37f030d0731471ec3fa05f61908d5b38db1e6c70
ryym <ryym.64@gmail.com>
2015-12-19 21:47:56 +0900

    Put winners back to entries when 'next()' called

--- src/core.js
+++ src/core.js
@@ -5,11 +5,22 @@ export function setEntries(state, entries) {
 }
 
 export function next(state) {
-  const entries = state.get('entries');
+  const winners = getWinners(state.get('vote'));
+  const entries = state.get('entries').concat(winners);
   return state.merge({
     vote: Map({ pair: entries.take(2) }),
     entries: entries.skip(2)
   });
+}
+function getWinners(vote) {
+  if(! vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if (aVotes === bVotes) {
+    return [a, b]
+  }
+  return (aVotes > bVotes) ? [a] : [b];
 }
 
 export function vote(state, entry) {
