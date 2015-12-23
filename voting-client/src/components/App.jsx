ce43dceeafd87f0e6945315af991c46cc1521d2e
ryym <ryym.64@gmail.com>
2015-12-23 16:33:34 +0900

    Results: Render entries and vote counts

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 // XXX: Hard coding for now.
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({ 'Trainspotting': 5, '28 Days Later': 4 });
 
 export default React.createClass({
   render() {
-    return React.cloneElement(this.props.children, { pair });
+    return React.cloneElement(this.props.children, {
+      pair, tally
+    });
   }
 });
--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 const Results = React.createClass({
+  mixins: [PureRenderMixin],
+
   render() {
+    const pair = this.props.pair || [];
     return (
-      <h1>Please implement me!</h1>
+      <div className="results">
+        {pair.map(this.renderEntry)}
+      </div>
     );
+  },
+
+  renderEntry(entry) {
+    return (
+      <div key={entry} className="entry">
+        <h1 className="entry-name">
+          {entry}
+        </h1>
+        <div className="vote-count">
+          {this.getVotes(entry)}
+        </div>
+      </div>
+    );
+  },
+
+  getVotes(entry) {
+    const { tally } = this.props;
+    return tally ? tally.get(entry, 0) : 0;
   }
 });
 
