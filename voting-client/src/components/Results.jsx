8d5e5e386ebf8ccb449a2795d4e5957047078816
ryym <ryym.64@gmail.com>
2015-12-23 14:19:23 +0900

    Add routing and results view

---
+++ voting-client/src/components/Results.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Results = React.createClass({
+  render() {
+    return (
+      <h1>Please implement me!</h1>
+    );
+  }
+});
+
+export default Results;
--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Router, { Route } from 'react-router';
+import App from './components/App';
+import Results from './components/Results';
 import Voting from './components/Voting';
 
-// XXX: Hard coding for now.
-const pair = [
-  'Trainspotting',
-  '28 Days Later'
-];
+const routes = (
+  <Route component={App}>
+    <Route path="/results" component={Results} />
+    <Route path="/" component={Voting} />
+  </Route>
+);
 
 render(
-  <Voting pair={pair} />,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
