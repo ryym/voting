50b659769b7a5b347777287aa287283a90b57baf
ryym <ryym.64@gmail.com>
2015-12-25 23:40:29 +0900

    Remove hasVoted if pair changed

--- voting-client/src/reducer.js
+++ voting-client/src/reducer.js
@@ -1,4 +1,4 @@
-import { Map } from 'immutable';
+import { List, Map } from 'immutable';
 
 function setState(state, action) {
   return state.merge(action.state);
@@ -12,10 +12,19 @@ function vote(state, entry) {
   return state;
 }
 
+function resetVote(state) {
+  const hasVoted = state.get('hasVoted');
+  const pair = state.getIn(['vote', 'pair'], List());
+  if (pair && ! pair.includes(hasVoted)) {
+    return state.remove('hasVoted');
+  }
+  return state;
+}
+
 export default function reducer(state = Map(), action) {
   switch(action.type) {
     case 'SET_STATE':
-      return setState(state, action);
+      return resetVote( setState(state, action) );
     case 'VOTE':
       return vote(state, action.entry);
   }
--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -68,6 +68,31 @@ describe('reducer', () => {
       }));
     });
 
+    it('removes hasVoted if pair changes', () => {
+      const initialState = fromJS({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: { 'Trainspotting': 1 }
+        },
+        hasVoted: 'Trainspotting'
+      });
+      const action = {
+        type: 'SET_STATE',
+        state: {
+          vote: {
+            pair: ['Sunshine', 'Slumdog Millionaire']
+          }
+        }
+      };
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: ['Sunshine', 'Slumdog Millionaire']
+        }
+      }));
+    });
+
   });
 
   describe('VOTE', () => {
