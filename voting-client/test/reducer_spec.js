dfdabc181c48d43f426b6057ba2809ed34b4d7e7
ryym <ryym.64@gmail.com>
2015-12-25 23:22:45 +0900

    Spec: Group SET_STATE tests

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -4,66 +4,70 @@ import reducer from '../src/reducer';
 
 describe('reducer', () => {
   
-  it('handles SET_STATE', () => {
-    const initialState = Map();
-    const action = {
-      type: 'SET_STATE',
-      state: Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later'),
-          tally: Map({ 'Trainspotting': 1 })
+  describe('SET_STATE', () => {
+
+    it('sets a new state', () => {
+      const initialState = Map();
+      const action = {
+        type: 'SET_STATE',
+        state: Map({
+          vote: Map({
+            pair: List.of('Trainspotting', '28 Days Later'),
+            tally: Map({ 'Trainspotting': 1 })
+          })
         })
-      })
-    };
-    const nextState = reducer(initialState, action);
+      };
+      const nextState = reducer(initialState, action);
 
-    expect(nextState).to.equal(fromJS({
-      vote: {
-        pair: [ 'Trainspotting', '28 Days Later' ],
-        tally: { 'Trainspotting': 1 }
-      }
-    }));
-  });
-
-  it('handles SET_STATE with plain JS payload', () => {
-    const initialState = Map();
-    const action = {
-      type: 'SET_STATE',
-      state: {
+      expect(nextState).to.equal(fromJS({
         vote: {
           pair: ['Trainspotting', '28 Days Later'],
           tally: { 'Trainspotting': 1 }
         }
-      }
-    };
-    const nextState = reducer(initialState, action);
+      }));
+    });
 
-    expect(nextState).to.equal(fromJS({
-      vote: {
-        pair: [ 'Trainspotting', '28 Days Later' ],
-        tally: { 'Trainspotting': 1 }
-      }
-    }));
-  });
+    it('sets a new state with plain JS payload', () => {
+      const initialState = Map();
+      const action = {
+        type: 'SET_STATE',
+        state: {
+          vote: {
+            pair: ['Trainspotting', '28 Days Later'],
+            tally: { 'Trainspotting': 1 }
+          }
+        }
+      };
+      const nextState = reducer(initialState, action);
 
-  it('handles SET_STATE without initial state', () => {
-    const action = {
-      type: 'SET_STATE',
-      state: {
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: [ 'Trainspotting', '28 Days Later' ],
+          tally: { 'Trainspotting': 1 }
+        }
+      }));
+    });
+
+    it('sets a new state without initial state', () => {
+      const action = {
+        type: 'SET_STATE',
+        state: {
+          vote: {
+            pair: ['Trainspotting', '28 Days Later'],
+            tally: { 'Trainspotting': 1 }
+          }
+        }
+      };
+      const nextState = reducer(undefined, action);
+
+      expect(nextState).to.equal(fromJS({
         vote: {
           pair: ['Trainspotting', '28 Days Later'],
           tally: { 'Trainspotting': 1 }
         }
-      }
-    };
-    const nextState = reducer(undefined, action);
+      }));
+    });
 
-    expect(nextState).to.equal(fromJS({
-      vote: {
-        pair: [ 'Trainspotting', '28 Days Later' ],
-        tally: { 'Trainspotting': 1 }
-      }
-    }));
   });
 
 });
