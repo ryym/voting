47f9506a517f22a3efe7e67e7d66d0728aed2693
ryym <ryym.64@gmail.com>
2015-12-26 20:23:33 +0900

    Apply middleware to sync with server

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Router, { Route } from 'react-router';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import io from 'socket.io-client';
 import reducer from './reducer';
 import { setState } from './action_creators';
+import remoteActionMiddleWare from './remote_action_middleware';
 import App from './components/App';
 import { ResultsContainer } from './components/Results';
 import { VotingContainer } from './components/Voting';
-
-const store = createStore(reducer);
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state => {
   store.dispatch( setState(state) );
 });
+
+const store = applyMiddleware(
+  remoteActionMiddleWare(socket)
+)(createStore)(reducer);
 
 const routes = (
   <Route component={App}>
